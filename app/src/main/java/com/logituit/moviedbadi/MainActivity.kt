c7c9ce07f5114bfa9f394ee45d05c3ccf70028e1
Aditya <aditya.rout@logituit.com>
2022-06-12 16:33:15 +0530

    initial android project added

---
+++ app/src/main/java/com/logituit/moviedbadi/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.logituit.moviedbadi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        // TODO handle night mode better
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
